e977e46fec93b4e1063baf031c6d89136b141923
Tim <tim@Karas-MacBook-Air.local>
2023-08-06 10:55:25 -0400

    change app name

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className="text-center page-container container">
-        <h1>Welcome to the App Name</h1>
+        <h1>Welcome to Positive Vibes</h1>
         <Link className="btn btn-primary" to="/survey">Start Survey</Link>
     </div>
   )
